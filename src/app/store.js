8b77cbbe4664420f1f56ec5086aa74f7a7e59d0e
Abylin Johnson <jgabylin@karunya.edu.in>
2021-10-02 22:48:29 +0530

    Home Page Completed

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,7 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
-
+import { configureStore } from "@reduxjs/toolkit";
+import carSlice from "../features/carSlides/carSlice";
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    car: carSlice,
   },
 });
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
-
+import CloseIcon from "@material-ui/icons/Close";
+import { selectCars } from "../features/carSlides/carSlice";
+import { useSelector } from "react-redux";
+import { Button } from "@material-ui/core";
 function Header() {
+  const [burgerStatus, setburgerStatus] = useState(true);
+  const cars = useSelector(selectCars);
+  console.log(cars);
   return (
     <Container>
       <a>
@@ -23,8 +29,51 @@ function Header() {
       <RightMenu>
         <a href="#">SHOP</a>
         <a href="#">ACCOUNT</a>
-        <a href="#">MENU</a>
       </RightMenu>
+      <MenuIcon>
+        <Button onClick={() => setburgerStatus(false)}>MENU</Button>
+      </MenuIcon>
+      <BurgerNav show={burgerStatus}>
+        <CloseWrapper>
+          <CustomClose onClick={() => setburgerStatus(true)} />
+        </CloseWrapper>
+        <li>
+          <a href="#">Existing Inventory</a>
+        </li>
+        <li>
+          <a href="#">Used Inventory</a>
+        </li>
+        <li>
+          <a href="#">Trade-in</a>
+        </li>
+        <li>
+          <a href="#">Cybertruck</a>
+        </li>
+        <li>
+          <a href="#">Roadster</a>
+        </li>
+        <li>
+          <a href="#">Semi</a>
+        </li>
+        <li>
+          <a href="#">Charging</a>
+        </li>
+        <li>
+          <a href="#">Powerwall</a>
+        </li>
+        <li>
+          <a href="#">Commercial Energy</a>
+        </li>
+        <li>
+          <a href="#">Utilities</a>
+        </li>
+        <li>
+          <a href="#">Find Us</a>
+        </li>
+        <li>
+          <a href="#">Support</a>
+        </li>
+      </BurgerNav>
     </Container>
   );
 }
@@ -37,10 +86,12 @@ const Container = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
+  margin-top: 10px;
   padding: 0 20px;
   top: 0;
   left: 0;
   right: 0;
+  z-index: 1;
 `;
 const Menu = styled.div`
   display: flex;
@@ -70,4 +121,57 @@ const RightMenu = styled.div`
     padding: 0 10px;
     flex-wrap: nowrap;
   }
+  @media (max-width: 768px) {
+    display: none;
+  }
+`;
+
+const BurgerNav = styled.div`
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  background-color: white;
+  width: 300px;
+  z-index: 16;
+  list-style: none;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  text-align: start;
+  transform: ${(props) =>
+    props.show ? "translateX(100%);" : "translateX(0);"};
+  transition: transform 0.2s ease-in-out;
+  li {
+    padding: 15px 0;
+
+    a {
+      font-weight: 300;
+    }
+  }
+`;
+
+const MenuIcon = styled.div`
+  display: flex;
+  align-items: center;
+  a {
+    font-weight: 500;
+    text-decoration: uppercase;
+    padding: 0 10px;
+    flex-wrap: nowrap;
+    padding: 5px;
+    font-weight: 600;
+    @media (max-width: 768px) {
+      background-color: rgb(0, 0, 0, 0.2);
+      border-radius: 50px;
+    }
+  }
+`;
+
+const CustomClose = styled(CloseIcon)`
+  cursor: pointer;
+`;
+const CloseWrapper = styled.div`
+  display: flex;
+  justify-content: flex-end;
 `;
